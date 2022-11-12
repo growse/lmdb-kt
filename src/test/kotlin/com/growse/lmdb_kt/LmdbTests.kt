1a53c2649b77ac7f38027caca673558b27719ea5
Andrew Rowson <github@growse.com>
2022-11-12 14:48:45 +0000

    Added in android test database

--- src/test/kotlin/com/growse/lmdb_kt/LmdbTests.kt
+++ src/test/kotlin/com/growse/lmdb_kt/LmdbTests.kt
@@ -17,7 +17,7 @@ import kotlin.test.assertTrue
 
 class LmdbTests {
     init {
-        System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "TRACE");
+        System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "TRACE")
     }
 
     @Test
@@ -31,7 +31,7 @@ class LmdbTests {
     @MethodSource("databasesWithStats")
     fun `given an environment, when fetching the stats, then the correct pagesize is detected`(
         dbPath: String,
-        expectedPageSize: Int,
+        expectedPageSize: Int
     ) {
         Environment(Paths.get(javaClass.getResource(dbPath)!!.toURI())).use { env ->
             env.stat.run {
@@ -39,7 +39,6 @@ class LmdbTests {
             }
         }
     }
-
 
     @ParameterizedTest
     @MethodSource("databasesWithStats")
@@ -67,23 +66,28 @@ class LmdbTests {
     @ParameterizedTest(name = "{index}: {0}")
     @MethodSource("databasesWithKeysValues")
     fun `given an environment, when dumping the data, then the correct key-values are returned`(
-        dbPath: String, pageSize: Int, expected: Map<String, LengthAndDigest>
+        dbPath: String,
+        pageSize: Int,
+        expected: Map<ByteArray, LengthAndDigest>
     ) {
         Environment(Paths.get(javaClass.getResource(dbPath)!!.toURI()), pageSize.toUInt()).use { env ->
             env.dump().run {
                 assertEquals(expected.size, size, "Entry count")
                 expected.keys.forEach {
-                    assertTrue(keys.contains(it), "Key exists in dump")
-                    assertEquals(expected[it]!!.length, this[it]!!.size, "Value size is correct for $it")
-                    assertEquals(expected[it]!!.digest, this[it]!!.digest())
+                    assertTrue(keys.contains(String(it)), "Key exists in dump")
+                    assertEquals(expected[it]!!.length, this[String(it)]!!.size, "Value size is correct for $it")
+                    assertEquals(expected[it]!!.digest, this[String(it)]!!.digest())
                 }
             }
         }
     }
 
     companion object {
+        /**
+         * Test parameters for each test database, listing the path and the stats for that db
+         */
         @JvmStatic
-        fun databasesWithStats(): Stream<Arguments> =/*
+        fun databasesWithStats(): Stream<Arguments> = /*
             dbPath: String,
             expectedPageSize: Int,
             expectedTreeDepth: Int,
@@ -102,59 +106,83 @@ class LmdbTests {
                 arguments("/databases/little-endian/4KB-page-size/single-large-value", 4096, 1, 0L, 1L, 9L, 1L),
                 arguments("/databases/little-endian/4KB-page-size/100-random-values", 4096, 3, 3L, 16L, 130L, 100L),
                 arguments("/databases/little-endian/4KB-page-size/single-entry-after-deleted", 4096, 1, 0L, 1L, 0L, 1L),
+                arguments("/databases/little-endian/android", 4096, 1, 0L, 1L, 0L, 2L)
             )
 
+        /**
+         * Test parameters for each test database, the path and the values that are contained
+         */
         @JvmStatic
-        fun databasesWithKeysValues(): Stream<Arguments> =/*
-            dbPath: String, expectedNumberOfEntries: Int, expected
+        fun databasesWithKeysValues(): Stream<Arguments> = /*
+            dbPath: String,
+            expectedNumberOfEntries: Int,
+            expected
              */
             Stream.of(
                 arguments("/databases/little-endian/16KB-page-size/empty", 16384, emptyMap<String, LengthAndDigest>()),
                 arguments(
                     "/databases/little-endian/16KB-page-size/single-entry",
                     16384,
-                    mapOf("KK123KK" to LengthAndDigest(8, "d2781036b05df83b95dd20f5a497102b"))
+                    mapOf("KK123KK".toByteArray() to LengthAndDigest(8, "d2781036b05df83b95dd20f5a497102b"))
                 ),
                 arguments(
                     "/databases/little-endian/16KB-page-size/single-large-value",
                     16384,
-                    mapOf("KK123KK" to LengthAndDigest(33000, "79a965574a648d48fc612f28cc49e570"))
+                    mapOf("KK123KK".toByteArray() to LengthAndDigest(33000, "79a965574a648d48fc612f28cc49e570"))
                 ),
-                arguments("/databases/little-endian/16KB-page-size/100-random-values", 16384, Paths.get(
-                    this::class.java.getResource("/100-key-values.csv")!!.toURI()
-                ).bufferedReader().lines().asSequence().associate {
-                    it.split(",").run {
-                        this[0].trim() to LengthAndDigest(this[1].trim().toInt(), this[2].trim())
+                arguments(
+                    "/databases/little-endian/16KB-page-size/100-random-values",
+                    16384,
+                    Paths.get(
+                        this::class.java.getResource("/100-key-values.csv")!!.toURI()
+                    ).bufferedReader().lines().asSequence().associate {
+                        it.split(",").run {
+                            this[0].toByteArray() to LengthAndDigest(this[1].trim().toInt(), this[2].trim())
+                        }
                     }
-                }),
+                ),
                 arguments("/databases/little-endian/4KB-page-size/empty", 4096, emptyMap<String, LengthAndDigest>()),
                 arguments(
                     "/databases/little-endian/4KB-page-size/single-entry",
                     4096,
-                    mapOf("KK123KK" to LengthAndDigest(8, "d2781036b05df83b95dd20f5a497102b"))
+                    mapOf("KK123KK".toByteArray() to LengthAndDigest(8, "d2781036b05df83b95dd20f5a497102b"))
                 ),
                 arguments(
                     "/databases/little-endian/4KB-page-size/single-large-value",
                     4096,
-                    mapOf("KK123KK" to LengthAndDigest(33000, "79a965574a648d48fc612f28cc49e570"))
+                    mapOf("KK123KK".toByteArray() to LengthAndDigest(33000, "79a965574a648d48fc612f28cc49e570"))
                 ),
-                arguments("/databases/little-endian/4KB-page-size/100-random-values", 4096, Paths.get(
-                    this::class.java.getResource("/100-key-values.csv")!!.toURI()
-                ).bufferedReader().lines().asSequence().associate {
-                    it.split(",").run {
-                        this[0].trim() to LengthAndDigest(this[1].trim().toInt(), this[2].trim())
+                arguments(
+                    "/databases/little-endian/4KB-page-size/100-random-values",
+                    4096,
+                    Paths.get(
+                        this::class.java.getResource("/100-key-values.csv")!!.toURI()
+                    ).bufferedReader().lines().asSequence().associate {
+                        it.split(",").run {
+                            this[0].toByteArray() to LengthAndDigest(this[1].trim().toInt(), this[2].trim())
+                        }
                     }
-                }),
+                ),
                 arguments(
                     "/databases/little-endian/4KB-page-size/single-entry-after-deleted",
                     4096,
-                    mapOf("KK123KK" to LengthAndDigest(8, "d2781036b05df83b95dd20f5a497102b"))
+                    mapOf("KK123KK".toByteArray() to LengthAndDigest(8, "d2781036b05df83b95dd20f5a497102b"))
                 ),
+                arguments(
+                    "/databases/little-endian/android",
+                    4096,
+                    mapOf(
+                        byteArrayOf(0, 0, 0, 0, 0, 0, 0, 0) to LengthAndDigest(224, "92ac74587530bc80322bffe124720811"),
+                        byteArrayOf(0, 0, 0, 0, 0, 0, 0, 1) to LengthAndDigest(744, "989463ef7b13d183dfda5bb08c47d37b")
+                    )
+                )
             )
     }
 }
 
 private fun ByteArray.digest(): String = MessageDigest.getInstance("MD5").digest(this).toHex()
 
-
+/**
+ * A helper class that represents a database record value concicsely for test parameters
+ */
 data class LengthAndDigest(val length: Int, val digest: String)
