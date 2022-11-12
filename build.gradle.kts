98a74b3edb7d7b8426890eaf913ea75689552d2c
Andrew Rowson <github@growse.com>
2022-11-12 14:11:09 +0000

    Added maven publish gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
+    id("maven-publish")
 }
 
 group = "com.growse"
@@ -24,4 +25,25 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/growse/lmdb-kt")
+            credentials {
+                username = System.getenv("GITHUB_USERNAME")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group.toString()
+            artifactId = "lmdb-kt"
+            version = version
+            from(components["java"])
+        }
+    }
 }
